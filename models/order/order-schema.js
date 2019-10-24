f764f73574dcdc9f10102dc0d698092934886b8f
Ahmed Mamdouh <pevolution.ahmed@gmail.com>
2019-10-24 11:23:41 +0200

    First commit

--- models/order/order-schema.js
+++ models/order/order-schema.js
@@ -1,6 +1,6 @@
 let Joi = require('@hapi/joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const joiProductSchema = require('../Product/product-schema');
+const joiProductSchema = require('../prod/product-schema');
 module.exports = Joi.object({
     userId    : Joi.objectId().required(),
     quantity  : Joi.number().integer().default(0).required(),
